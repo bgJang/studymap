3fd43a0571f9fc28f7c55e01b825c555f582f4da
bogyeongjang <bogyeongjang@koreanair.com>
2023-05-09 15:29:55 +0900

    layout 크기 변경 & Source, Target 방향 변경

--- src/index.js
+++ src/index.js
@@ -30,27 +30,96 @@ import './style.css';
 const data = [ // list of graph elements to start with
     {   // node : C++
         "data": {
-            "id": 'cpp_title',
-             "url": '',
-            "label": 'C++'
+            "id": 'cpp',
+            "url": '',
+            "label": 'C++', 
+            "rank" : 1
         }
     }, 
+    {
+        // C++ 스터디 
+        "data": {
+            "id": 'cpp_study',
+            "url": '',
+            "label": 'C++ 스터디', 
+            "rank" : 2
+        }
+    },
+    {
+        // C++ 블로그 
+        "data": {
+            "id": 'cpp_blog',
+            "url": '',
+            "label": 'C++ 블로그', 
+            "rank" : 3
+        }
+    },
     {   // node : 씹어먹는 C++
         "data": {
             "id": 'cpp_modoo',
             "url": 'https://modoocode.com/135',
-            "label": '씹어먹는 C++'
+            "label": '씹어먹는 C++', 
+            "rank" : 5
         }
     }, 
 
-    {   // edge : C++->씹어먹는 C++
+    {   // C++ -> C++ 스터디
         "data": {
-            "id": 'cpp_title->cpp_modoo',
-            "source": 'cpp_title',
-            "target":'cpp_modoo'
+            "id": 'cpp_title->cpp_study',
+            "source": 'cpp_study',
+            "target":'cpp'
+        }
+    },    
+    {   // C++ -> C++ 블로그
+        "data": {
+            "id": 'cpp_title->cpp_blog',
+            "source": 'cpp_blog',
+            "target":'cpp'
+        }
+    },
+    {   // edge : C++ 스터디 ->씹어먹는 C++
+        "data": {
+            "id": 'cpp_study->cpp_modoo',
+            "source": 'cpp_modoo',
+            "target":'cpp_study'
         }
     }
 ];
+
+
+
+const cy_for_rank = cytoscape({
+    elements: data
+});
+// rank를 활용하기 위해 data만 입력한 cytoscape 객체입니다
+
+const pageRank = cy_for_rank.elements().pageRank();
+
+
+
+// 별도로 가중치를 주려 했는데 안예뻐서 일단 pass.
+
+// elements들의 rank들입니다.
+// Source로 이용되는 숫자에 따라 크기를 결정한다.
+// let pageRanks = [];
+
+// for(var i = 0; i < Object.keys(data).length; i++) {
+//     if(data[i]["data"].label) { 
+//         pageRanks[data[i]["data"].id] = data[i]["data"].rank;
+//     }
+// }
+// console.log(pageRanks);
+
+
+
+
+const nodeMaxSize = 50;
+const nodeMinSize = 5;
+const fontMaxSize = 8;
+const fontMinSize = 5;
+// 추후 마우스 인/아웃 시에도 활용해야 하니 node와 font의 최대값/최소값은 변수로 빼줍니다
+
+
 
 // 아래는 공식 사이트에 올라와 있는 예제 코드입니다
 var cy = cytoscape({
@@ -65,7 +134,20 @@ var cy = cytoscape({
             style: {
                 'background-color': '#666',
                 //'label': 'data(id)'
-                'label' : 'data(label)' // id 대신 label 표시
+                'label' : 'data(label)', // id 대신 label 표시
+                'width': function (ele) {
+                    // return nodeMaxSize * (5 - pageRanks[ele.id()]) + nodeMinSize;
+                    return nodeMaxSize *  pageRank.rank('#' + ele.id())  + nodeMinSize;
+
+                },
+                'height': function (ele) {
+                    // return nodeMaxSize *  (5 - pageRanks[ele.id()]) + nodeMinSize;
+                    return nodeMaxSize *  pageRank.rank('#' + ele.id()) + nodeMinSize;
+                },
+                'font-size': function (ele) {
+                    // return fontMaxSize * (5 - pageRanks[ele.id()]) + fontMinSize;
+                    return fontMaxSize *   pageRank.rank('#' + ele.id()) + fontMinSize;
+                }
             }
         },
 
@@ -74,15 +156,16 @@ var cy = cytoscape({
             style: {
                 'width': 3,
                 'line-color': '#ccc',
-                
                 // 화살표
-                'curve-style': 'bezier',
-
+                'curve-style': 'bezier', 
                 // 화살표 방향 Source -> Target
-                'target-arrow-color': '#ccc',
-                // 'target-arrow-shape': 'triangle'
-                'target-arrow-shape': 'vee'
+                // 'target-arrow-color': '#ccc', 
+                // // 'target-arrow-shape': 'triangle'
+                // 'target-arrow-shape': 'vee'
                 
+                // 화살표 방향 Source <- Target
+                'source-arrow-color': '#ccc',
+                'source-arrow-shape': 'vee'
             }
         }
     ],
